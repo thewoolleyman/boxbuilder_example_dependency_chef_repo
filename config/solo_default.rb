b9a052c3a21cd531db6e4828b8df78b34bd0907b
Chad Woolley <thewoolleyman@gmail.com>
2010-09-12 00:21:51 -0700

    fix rename missed typo

--- config/solo_default.rb
+++ config/solo_default.rb
@@ -4,5 +4,5 @@ file_store_path chef_dir
 log_level :debug
 Chef::Log::Formatter.show_time = false
 cookbook_path [
-  File.expand_path('boxbuilder_example_dependency_chef_repo/site-cookbooks',chef_dir)
+  File.expand_path('boxbuilder_example_dependency_chef_repo/cookbooks',chef_dir)
 ]
